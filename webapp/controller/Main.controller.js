ddb28ad8d019e090c0121d8d16637d98aa52cf71
ljs <gullamahi@gmail.com>
2017-01-26 17:01:30 +0000

    Work list added detail view

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/Device"
-], function(Controller, Device) {
+	"sap/ui/Device",
+	"sap/ui/model/Filter"
+], function(Controller, Device, Filter) {
 	"use strict";
 
 	return Controller.extend("ch81fullscreen.controller.Main", {
@@ -16,6 +17,41 @@ sap.ui.define([
 				sContentDensityClass = "sapUiSizeCozy";
 			}
 			this.getView().addStyleClass(sContentDensityClass);
+		},
+		onTableUpdateFinished: function(evt) {
+			var sTitle = "Sales Orders",
+				oTable = this.getView().byId("table");
+			//catch cases where the backend is not supporting remote count
+			if (oTable.getBinding("items").isLengthFinal()) {
+				var iCount = evt.getParameter("total"),
+					iItems = oTable.getItems().length;
+				sTitle += "(" + iItems + "/" + iCount + ")";
+
+			}
+
+			this.getView().byId("title").setText(sTitle);
+		},
+		onChange: function(oEvent) {
+			var sSearchValue = oEvent.getSource().getValue(),
+				aFilters = [];
+			if (sSearchValue.length > 0) {
+				var oFilterName = new Filter("CustomerName", sap.ui.model.FilterOperator.Contains, sSearchValue);
+				var oFilterID = new Filter("SalesOrderID", sap.ui.model.FilterOperator.Contains, sSearchValue);
+				aFilters.push(new Filter({
+					filters: [oFilterName, oFilterID],
+					And: false
+				}));
+
+			}
+			this.getView().byId("table").getBinding("items").filter(aFilters, "Application");
+
+		},
+
+		onItemPress: function(oEvent) {
+			var sSalesOrderId = oEvent.getSource().getBindingContext().getProperty("SalesOrderID");
+			this.getOwnerComponent().getRouter().navTo("salesOrder", {
+				SalesOrderID: sSalesOrderId
+			}, false);
 		}
 
 	});
